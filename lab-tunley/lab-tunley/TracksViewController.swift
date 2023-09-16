298a417eeeb6556fb5dc1c1540a1d74fc6484325
sdegefa <sdegefa2020@fau.edu>
2023-09-15 22:33:20 -0400

    Step 1 Fetch API Data Complete

--- lab-tunley/lab-tunley/TracksViewController.swift
+++ lab-tunley/lab-tunley/TracksViewController.swift
@@ -17,7 +17,41 @@ class TracksViewController: UIViewController, UITableViewDataSource {
         super.viewDidLoad()
 
         // TODO: Pt 1 - Set tracks property with mock tracks array
-        tracks = Track.mockTracks
+        // Create a URL for the request
+        // In this case, the custom search URL you created in in part 1
+        let url = URL(string: "https://itunes.apple.com/search?term=don't-waste-your-weeps")!
+
+        // Use the URL to instantiate a request
+        let request = URLRequest(url: url)
+
+        // Create a URLSession using a shared instance and call its dataTask method
+        // The data task method attempts to retrieve the contents of a URL based on the specified URL.
+        // When finished, it calls it's completion handler (closure) passing in optional values for data (the data we want to fetch), response (info about the response like status code) and error (if the request was unsuccessful)
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+
+            // Handle any errors
+            if let error = error {
+                print("❌ Network error: \(error.localizedDescription)")
+            }
+
+            // Make sure we have data
+            guard let data = data else {
+                print("❌ Data is nil")
+                return
+            }
+
+            // The `JSONSerialization.jsonObject(with: data)` method is a "throwing" function (meaning it can throw an error) so we wrap it in a `do` `catch`
+            // We cast the resultant returned object to a dictionary with a `String` key, `Any` value pair.
+            do {
+                let jsonDictionary = try JSONSerialization.jsonObject(with: data) as? [String: Any]
+                print(jsonDictionary)
+            } catch {
+                print("❌ Error parsing JSON: \(error.localizedDescription)")
+            }
+        }
+
+        // Initiate the network request
+        task.resume()
         print(tracks)
 
         tableView.dataSource = self
